3fe00700b0fdc000657721e8848d1687e41fce48
Oliver Ton Sandoval Arevalo <elunicorm64@gmail.com>
2019-08-17 07:51:20 -0500

    menores

--- apps/principal/admin.py
+++ apps/principal/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Formulario
+from .models import Formulario,Persona
 
 admin.site.register(Formulario)
+admin.site.register(Persona)
--- apps/principal/models.py
+++ apps/principal/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length = 100)
+    apellido = models.CharField(max_length = 150)
+    correo = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.nombre
+
+
 
 class Formulario(models.Model):
     id = models.AutoField(primary_key = True)
     nombre = models.CharField('Nombre', max_length = 100)
     apellido = models.CharField('Apellido', max_length = 150)
     asunto = models.CharField('Asunto', max_length = 200)
+    referente = models.ForeignKey(Persona, on_delete = models.CASCADE, null = True, blank = True)
     email = models.EmailField('Email', max_length = 150)
     mensaje = models.TextField('Mensaje')
 
@@ -14,5 +25,5 @@ class Formulario(models.Model):
         verbose_name = 'Formulario'
         verbose_name_plural = 'Formularios'
 
-    def _str__(self):
+    def __str__(self):
         return self.asunto
